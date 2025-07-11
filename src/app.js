a72b2dcf760bbd6108b0ab9c64694f46bf15e848
Tyrone L. Copeland Jr. <tycopelandjr@gmail.com>
2025-07-11 16:12:24 +0000

    Document Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let extensions = ['.com', '.net', '.io'];
+
+  let domainNames = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < extensions.length; l++) {
+          let domain = pronoun[i] + adj[j] + noun[k] + extensions[l];
+          domainNames.push(domain);
+        }
+      }
+    }
+
+     
+  }
+
+let output = document.getElementById("domain-output");
+  output.innerHTML = domainNames.map(name => `<li>${name}</li>`).join("");
+  console.log(domainNames);
+  
 };
+
